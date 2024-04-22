131a26206e8889b925ab363480c405cc7da8cd56
Nikhil K <nikhil.karanje90@gmail.com>
2024-04-22 14:37:17 +0530

    Package 2 Lab 2 Simple Addition Subtraction program

---
+++ src/exde_22042024/Lab004.java
@@ -0,0 +1,11 @@
+package exde_22042024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+
+        String firstName = "Nikhil";
+        String lastName = "K";
+        System.out.print(firstName +"\t" + lastName);
+
+    }
+}
