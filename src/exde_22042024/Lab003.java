ad897b1734183c5d9c7dc4f4e3b2eb039f87af54
Nikhil K <nikhil.karanje90@gmail.com>
2024-04-22 11:17:40 +0530

    Package 2 Lab 2 Simple Addition Subtraction program

---
+++ src/exde_22042024/Lab003.java
@@ -0,0 +1,17 @@
+package exde_22042024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Hello Nikhil");
+        int age = 33;
+        System.out.println("My age is --> " +age);
+
+        System.out.printf("My age is %d", age);
+        System.out.println();
+        int a = 22, b = 33;
+
+        System.out.println(a+b);
+        System.out.println("Addition =\t" +(a+b));
+
+    }
+}
