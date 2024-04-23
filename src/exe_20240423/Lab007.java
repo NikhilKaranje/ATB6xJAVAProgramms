f9a4acbd6520f2891720ddbf05624477203ff2c1
Nikhil K <nikhil.karanje90@gmail.com>
2024-04-23 16:59:40 +0530

    Package 3 Lab 6 & 7 increment operator

---
+++ src/exe_20240423/Lab007.java
@@ -0,0 +1,16 @@
+package exe_20240423;
+
+public class Lab007 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println(++a + ++a + a++);
+
+        // part 1 --> ++a --> Expression = 11, value of a = 11
+        // part 2 --> ++a --> Expression = 12, value of a = 12
+        // part 3 --> a++ --> Expression = 12, value of a = 13
+        //addition of expressions (11+12+12) =
+
+        System.out.println(a);
+    }
+}
