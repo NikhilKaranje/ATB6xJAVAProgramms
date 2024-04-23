1b4f6539b3ac59a26f9f07693bfc52695ae12ba2
Nikhil K <nikhil.karanje90@gmail.com>
2024-04-23 16:45:27 +0530

    Package 3 Lab 4 and 5 Simple Addition Subtraction program

---
+++ src/exe_20240423/Lab006.java
@@ -0,0 +1,26 @@
+package exe_20240423;
+
+public class Lab006 {
+    public static void main(String[] args) {
+        //Increment operator
+        int a = 10;
+        int b = ++a;
+
+        System.out.println("value of a = " +a);
+        System.out.println("value of b = " +b);
+
+        System.out.println("---------->");
+
+        int c = a++;
+
+        System.out.println("value of a = " +a);
+        System.out.println("value of b = " +b);
+        System.out.println("value of c = " +c);
+
+        System.out.println("----------->");
+
+        int d = 10;
+
+        System.out.println(++d + d++);
+    }
+}
