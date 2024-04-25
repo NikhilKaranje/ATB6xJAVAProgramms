0c09dfa4b0167c31fbd20d176726a11e846a7c79
Nikhil K <nikhil.karanje90@gmail.com>
2024-04-25 18:52:04 +0530

    String Lab 7 & 8

---
+++ src/exe_20240425/Lab008.java
@@ -0,0 +1,14 @@
+package exe_20240425;
+
+import java.util.Locale;
+
+public class Lab008 {
+    public static void main(String[] args) {
+
+        String myName = "nikhil";
+        String lastName = "k";
+
+        System.out.println(myName +  lastName);
+        System.out.println(myName.toUpperCase()+ "\t" + lastName.toUpperCase());
+    }
+}
