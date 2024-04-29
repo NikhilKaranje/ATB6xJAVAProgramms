32c54499dbdc9c637010684651a1d93379739cb8
Nikhil K <nikhil.karanje90@gmail.com>
2024-04-29 15:20:37 +0530

    Ternary Operator
    Lab 009
    Max/Min from 3 numbers

---
+++ src/exe_20240429/Lab009.java
@@ -0,0 +1,25 @@
+package exe_20240429;
+
+public class Lab009 {
+    public static void main(String[] args) {
+
+        //Max Min number with Ternary operator
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int max = a < b  ? b : a ;
+        int min = a < b ? a : b ;
+
+        System.out.println("Max number is = "+ max);
+        System.out.println("Min number is = "+ min);
+
+        //Three numbers max -
+
+        int max1 = (a>b && a>c) ? a: (b>a && b>c) ? b: c ;
+        int min1 = (a<b && a<c) ? a: (b<a && b<c) ? b:c ;
+
+        System.out.println("Largest Number is "+ max1);
+        System.out.println("Smallest number is "+ min1);
+    }
+}
