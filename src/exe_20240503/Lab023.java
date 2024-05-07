ac9c0e797804c429c4bf3736485de8de986bc036
Nikhil K <nikhil.karanje90@gmail.com>
2024-05-07 21:59:02 +0530

    Switch programs

--- src/exe_20240503/Lab023.java
+++ src/exe_20240503/Lab023.java
@@ -1,2 +1,26 @@
-package exe_20240503;public class Lab023 {
+package exe_20240503;
+
+import java.util.Scanner;
+
+public class Lab023 {
+
+    public static void main(String[] args) {
+
+        // 3/x^2 + y^2 + |z|
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of x = ");
+        double x = sc.nextDouble();
+        System.out.println("Enter value of y = ");
+        double y = sc.nextDouble();
+        System.out.println("Enter value of z = ");
+        double z =  sc.nextDouble();
+
+        double result;
+
+        result = Math.cbrt(Math.pow(x,2) + Math.pow(y,2) - Math.abs(z));
+
+        System.out.println(result);
+        sc.close();
+    }
 }
--- src/exe_20240503/Lab024.java
+++ src/exe_20240503/Lab024.java
@@ -1,2 +1,28 @@
-package exe_20240503;public class Lab024 {
+package exe_20240503;
+
+import java.util.Scanner;
+
+public class Lab024 {
+    public static void main(String[] args) {
+
+        //Basic Info details
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name = ");
+        String name = sc.nextLine();
+
+        System.out.println("Enter your Age = ");
+        int age = sc.nextInt();
+
+        System.out.println("Enter your salary = ");
+        double slr = sc.nextDouble();
+
+        System.out.println("-----------------------");
+
+        System.out.println("Name is " +name);
+        System.out.println("Age is " +age);
+        System.out.println("Salary is "+slr);
+
+    }
 }
