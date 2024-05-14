5d09c5ae1dc832986369426ec91f0431f6a29aa5
Nikhil K <nikhil.karanje90@gmail.com>
2024-05-14 16:08:44 +0530

    Array programs

--- src/May/Lab031.java
+++ src/May/Lab031.java
@@ -3,7 +3,13 @@ package May;
 public class Lab031 {
     public static void main(String[] args) {
 
-        //Array
+        //Double the Array
 
+        int[] salaries = {100,250,220, 30,490};
+
+        for(int i=0; i< salaries.length; i++){
+            System.out.println("Your salary was = "+ salaries[i]+ "K");
+            System.out.println("--> After appraisal = "+ salaries[i]*2+"K");
+        }
     }
 }
---
+++ src/May/Lab032.java
@@ -0,0 +1,28 @@
+package May;
+
+import java.util.Scanner;
+
+public class Lab032 {
+    public static void main(String[] args) {
+        //Using Array
+        //Enter marks
+        //Find failed subject
+
+        Scanner sc = new Scanner(System.in);
+        float[] marks = new float[3];
+        System.out.println("Enter marks for Sub1 = ");
+        marks[0] = sc.nextFloat();
+        System.out.println("Enter marks for Sub 2 = ");
+        marks[1] = sc.nextFloat();
+        System.out.println("Enter marks for Sub 3 = ");
+        marks[2] = sc.nextFloat();
+
+        for(int i = 0; i< marks.length; i++){
+            if (marks[i]<= 34){
+                System.out.println("Failed in subject "+ marks[i]);
+            }else {
+                System.out.println("Marks are = " +marks[i]);
+            }
+        }
+    }
+}
