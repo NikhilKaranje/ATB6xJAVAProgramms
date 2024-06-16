2fb38bf1e0a4da1da726d72dd9fefba43b9bf92b
Nikhil K <nikhil.karanje90@gmail.com>
2024-06-16 16:59:22 +0530

    ArrayList - Clooewction Framework

---
+++ src/June/exe_20241506/Lab47.java
@@ -0,0 +1,20 @@
+package June.exe_20241506;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab47 {
+
+    //Collection framework - Arraylist
+
+    public static void main(String[] args) {
+
+        List<String> myList = new ArrayList<>();
+
+        myList.add("Nikhil");
+        myList.add("Vinit");
+        myList.add("Pushkar");
+
+        System.out.println(myList);
+    }
+}
